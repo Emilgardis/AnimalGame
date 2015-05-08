16f95c18cb335ade097e60760a57d6796cb8daa3
Emil Gardström <emil.gardstrom@gmail.com>
2015-05-08 12:11:17 +0200

    Fixed game.py's animal.txt file handling and added neater animal structure

--- game.py
+++ game.py
@@ -1,4 +1,14 @@
-"""Challenge #212 Animal Guess game"""
+"""Challenge #212 Animal Guess game
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import sys
 import os
 import json
@@ -16,6 +26,14 @@ class Animal(object):
         self.a_has = animal_has  # animal has
         self.a_is = animal_is  # animal is
 
+    @classmethod
+    def loadjs(cls, a_data):
+        """Used for loading animal from dict"""
+        cls.a_has = a_data.items()[0][1]["has"]
+        cls.a_is = a_data.items()[0][1]["is"]
+        cls.a_name = a_data.items()[0][0]
+        return cls
+
     def add_has(self, key, value=True):
         self.a_has.update({key: value})
 
@@ -27,12 +45,35 @@ class Animal(object):
         return {self.a_name: data}
 
 
+class GuessAnimal(Animal):
+
+    """Class for the animal that is guessed"""
+
+    def __init__(self):
+        super(GuessAnimal, self).__init__("Guess")
+        self.state = False
+        self.matches = {}
+
+    def get_matches(self):
+        with open(ANIMALSRC, "r") as data_file:
+            data = json.load(data_file)
+        print data
+        matching = {}
+        for animal in data:
+            animal = Animal.loadjs(animal)
+            # Elements matches
+            has_match = dict(set(animal.a_has.items()) & set(self.a_has.items()))
+            is_match = dict(set(animal.a_is.items()) & set(self.a_is.items()))
+            matching[animal.a_name] = {"has": has_match, "is": is_match}
+        return matching
+
+
 class Game(object):
 
     def __init__(self):
         """initialize Game"""
         super(Game, self).__init__()
-        self.state = None
+        self.filestate = [None,None]
         self.current = {}
         self.questions = self.load_questions()
         self.animals = self.load_animals()
@@ -40,22 +81,28 @@ class Game(object):
     def load_questions(self):
         try:
             with open(QUESTIONSRC, "r") as data_file:
+                self.filestate[0] == True
                 return json.load(data_file)
         except:
             print "Cannot open json file {}".format(QUESTIONSRC)
-            return {"has": [], "is": []}
+            self.filestate[0] = False
+            return {"has": {}, "is": {}}
 
     def load_animals(self):
         try:
             with open(ANIMALSRC, "r") as data_file:
+                self.filestate[1] = True
                 return json.load(data_file)
         except:
             print "Cannot open json file {}".format(ANIMALSRC)
+            self.filestate[1] = False
             return []
 
     def add_question(self, key, question):
         assert key in ("has", "is")
         self.questions[key].append(question)
+        if not self.filestate[0]:
+            return
         with open(QUESTIONSRC, "w") as data_file:
             json.dump(
                 self.questions,
@@ -67,6 +114,8 @@ class Game(object):
 
     def add_animal(self, animal):
         self.animals.append(animal.get())
+        if not self.filestate[1]:
+            return
         with open(ANIMALSRC, "w") as data_file:
             json.dump(
                 self.animals,
@@ -75,3 +124,22 @@ class Game(object):
                 sort_keys=True,
                 separators=(',', ':'),
             )
+
+    def gameLoop(self):
+        pass
+
+if __name__ == "__main__":
+    g = Game()
+    dog = Animal("rat")
+    print("dog name: {}".format(dog.a_name))
+    print("dog has:\n{}".format(dog.a_has))
+    print("dog is:\n{}".format(dog.a_is))
+    g.add_animal(dog)
+    cat = g.animals["cat"]
+    print("cat name: {}".format(cat.a_has))
+    guess = GuessAnimal() # Cat
+    guess.add_has("tail")
+    guess.add_has("paws")
+    guess.add_is("curious")
+    print("matches:\n{}".format(guess.get_matches))
+
